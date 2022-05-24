619aeef3d22b5adeea16a5f10a6d756ab279a312
gilmermamani <gilmer.mamani@upeu.edu.pe>
2022-05-24 17:57:26 +0000

    EstructuraRepetitiva

--- EstructuraRepetitiva/EjerciciosVarios.java
+++ EstructuraRepetitiva/EjerciciosVarios.java
@@ -1,5 +1,6 @@
 package EstructuraRepetitiva;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class EjerciciosVarios {
@@ -29,13 +30,36 @@ public class EjerciciosVarios {
         //System.out.println("El factorial de "+n+" es:"+result);
         return result;
     }
-
-
-
-
-
+    public static BigInteger factorialnumBig(int n) {
+        BigInteger result=new BigInteger("1");
+   
+        if(n>1){
+        for (int i = 1; i <=n; i++) {
+            result=result.multiply(new BigInteger(String.valueOf(i))); 
+            }
+        }
+        
+        return result;
+    }
+    public static void funcionExponencial(int x) {
+        double result=0
+        for(int i = 0; i < x; i++) {
+            result=result+Math.pow(x, i)/factorialnum(i));
+        }
+        
+    }
 
     public static void main(String[] args) {
-        factorialXnum();
+       /* factorialXnum();
+        long resultado=factorialnum(5);
+        System.out.println("Resultado:"+resultado);*/
+        System.out.println("Ingrese un numero:");
+        int numero=leerT.nextInt();
+
+        for (int i = 1; i <=numero; i++) {
+            System.out.println("Factorial de "+i+"! =" + factorialnumBig(i));
+        }
+            
+        
     }
 }
