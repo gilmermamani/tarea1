052bd6508eabb890d6268bf781f40d561d0a9f96
gilmermamani <gilmer.mamani@upeu.edu.pe>
2022-05-24 16:21:20 +0000

    farial

--- EstructuraRepetitiva/EjerciciosVarios.java
+++ EstructuraRepetitiva/EjerciciosVarios.java
@@ -18,6 +18,23 @@ public class EjerciciosVarios {
         System.out.println("El factorial de "+n+" es:"+result);
     }
 
+    public static long factorialnum(int n) {
+        long result=1; 
+   
+        if(n>1){
+        for (int i = 1; i <=n; i++) {
+            result=result*i; 
+            }
+        }
+        //System.out.println("El factorial de "+n+" es:"+result);
+        return result;
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         factorialXnum();
     }
